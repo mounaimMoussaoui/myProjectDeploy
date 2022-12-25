c933a9cb5b397ad8f1f7224b90af171223226180
mounaimMoussaoui <moussaouiabdennaim@gmail.com>
2022-12-25 20:00:45 +0100

    add comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 import Layout from './pages/Layout'
 
 function App() {
   return (
     <div className="App">
     <Layout />
-      <header className="App-header flex w-full h-[80px] bg-black text-white justify-between items-center px-[20px]">
+      {/* <header className="App-header flex w-full h-[80px] bg-black text-white justify-between items-center px-[20px]">
         <img src={logo} className="App-logo w-[70px] h-[70px]" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
 
     </div>
   );
