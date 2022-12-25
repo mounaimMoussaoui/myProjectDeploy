cffed9c2effdbf326cd16101f9c976a4846a210d
mounaimMoussaoui <moussaouiabdennaim@gmail.com>
2022-12-25 20:26:19 +0100

    change router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-// import logo from './logo.svg';
-// import './App.css';
-import Layout from './pages/Layout'
+import Home from '../src/pages/Home';
+import Contact from '../src/pages/Contact';
+import About from '../src/pages/About';
+import Portfolio from '../src/pages/Portfolio';
+import './App.css';
+// import Layout from './pages/Layout'
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import {AiOutlineHome, AiOutlineContacts, AiOutlineFundProjectionScreen, AiOutlineUser} from "react-icons/ai";
+
 
 function App() {
   return (
     <div className="App">
-    <Layout />
-      {/* <header className="App-header flex w-full h-[80px] bg-black text-white justify-between items-center px-[20px]">
-        <img src={logo} className="App-logo w-[70px] h-[70px]" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="text-[#eee]"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
+      <BrowserRouter basename="/my-project">
+        <Routes>
+        <nav className="shadow min-h-[100vh] p-2">
+            <ul className="nav-bar flex flex-col gap-4">
+            <li className="px-4 active">
+                  <Route exact path="/my-project" element={<Home />} >
+                    <NavLink to="/" className="flex items-center gap-3">
+                      <AiOutlineHome className="w-[25px] h-[25px]" />
+                      <span className="hidden md:block">Home</span>
+                    </NavLink>
+                  </Route>
+            </li>
+            <li className="px-4">
+              <Route path="/about" element={<About />} >
+                <NavLink to={"/about"} className="flex items-center gap-3 whitespace-nowrap">
+                    <AiOutlineUser className="w-[25px] h-[25px]" />
+                    <span className="hidden md:block">About Me</span>
+                </NavLink>
+              </Route>
+            </li>
+            <li className="px-4">
+              <Route path="/contact" element={<Contact />} >
+                <NavLink to="/contact" className="flex items-center gap-3">
+                    <AiOutlineContacts className="w-[25px] h-[25px]" />
+                    <span className="hidden md:block">Contact</span>
+                </NavLink>
+              </Route>
+            </li>
+            {/* <li>
+                <Link to="/skills" className="flex items-center gap-3">
+                    <GiSkills />
+                    Skills
+                </Link>
+            </li> */}
+            <li className="px-4">
+              <Route path="/portfolio" element={<Portfolio />} >
+                <NavLink to="/portfolio" className="flex items-center gap-3">
+                    <AiOutlineFundProjectionScreen className="w-[25px] h-[25px]" />
+                    <span className="hidden md:block">portfolio</span>
+                </NavLink>
+              </Route>
+            </li>
+            </ul>
+        </nav>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,49 +1,49 @@
-import { Outlet,  NavLink } from "react-router-dom";
-import {AiOutlineHome, AiOutlineContacts, AiOutlineFundProjectionScreen, AiOutlineUser} from "react-icons/ai";
-// import {GiSkills} from "react-icons/gi";
-import "./style/navBar.css"
+// import { Outlet,  NavLink } from "react-router-dom";
+// import {AiOutlineHome, AiOutlineContacts, AiOutlineFundProjectionScreen, AiOutlineUser} from "react-icons/ai";
+// // import {GiSkills} from "react-icons/gi";
+// import "./style/navBar.css"
 
-const Layout = () => {
-    return (
-        <>
-        <nav className="shadow min-h-[100vh] p-2">
-            <ul className="nav-bar flex flex-col gap-4">
-            <li className="px-4 active">
-                <NavLink to="/" className="flex items-center gap-3">
-                    <AiOutlineHome className="w-[25px] h-[25px]" />
-                    <span className="hidden md:block">Home</span>
-                </NavLink>
-            </li>
-            <li className="px-4">
-                <NavLink to={"/about"} className="flex items-center gap-3 whitespace-nowrap">
-                    <AiOutlineUser className="w-[25px] h-[25px]" />
-                    <span className="hidden md:block">About Me</span>
-                </NavLink>
-            </li>
-            <li className="px-4">
-                <NavLink to="/contact" className="flex items-center gap-3">
-                    <AiOutlineContacts className="w-[25px] h-[25px]" />
-                    <span className="hidden md:block">Contact</span>
-                </NavLink>
-            </li>
-            {/* <li>
-                <Link to="/skills" className="flex items-center gap-3">
-                    <GiSkills />
-                    Skills
-                </Link>
-            </li> */}
-            <li className="px-4">
-                <NavLink to="/portfolio" className="flex items-center gap-3">
-                    <AiOutlineFundProjectionScreen className="w-[25px] h-[25px]" />
-                    <span className="hidden md:block">portfolio</span>
-                </NavLink>
-            </li>
-            </ul>
-        </nav>
+// const Layout = () => {
+//     return (
+//         <>
+//         <nav className="shadow min-h-[100vh] p-2">
+//             <ul className="nav-bar flex flex-col gap-4">
+//             <li className="px-4 active">
+//                 <NavLink to="/" className="flex items-center gap-3">
+//                     <AiOutlineHome className="w-[25px] h-[25px]" />
+//                     <span className="hidden md:block">Home</span>
+//                 </NavLink>
+//             </li>
+//             <li className="px-4">
+//                 <NavLink to={"/about"} className="flex items-center gap-3 whitespace-nowrap">
+//                     <AiOutlineUser className="w-[25px] h-[25px]" />
+//                     <span className="hidden md:block">About Me</span>
+//                 </NavLink>
+//             </li>
+//             <li className="px-4">
+//                 <NavLink to="/contact" className="flex items-center gap-3">
+//                     <AiOutlineContacts className="w-[25px] h-[25px]" />
+//                     <span className="hidden md:block">Contact</span>
+//                 </NavLink>
+//             </li>
+//             {/* <li>
+//                 <Link to="/skills" className="flex items-center gap-3">
+//                     <GiSkills />
+//                     Skills
+//                 </Link>
+//             </li> */}
+//             <li className="px-4">
+//                 <NavLink to="/portfolio" className="flex items-center gap-3">
+//                     <AiOutlineFundProjectionScreen className="w-[25px] h-[25px]" />
+//                     <span className="hidden md:block">portfolio</span>
+//                 </NavLink>
+//             </li>
+//             </ul>
+//         </nav>
 
-        <Outlet />
-        </>
-    )
-};
+//         <Outlet />
+//         </>
+//     )
+// };
 
-export default Layout;
+// export default Layout;
