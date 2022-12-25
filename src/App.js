abd9793a8577b37acd93f20377f400133d8be1e3
mounaimMoussaoui <moussaouiabdennaim@gmail.com>
2022-12-25 18:59:09 +0100

    deploy app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 
 import Layout from './pages/Layout'
 
@@ -7,21 +7,6 @@ function App() {
   return (
     <div className="App">
     <Layout />
-      {/* <header className="App-header flex w-full h-[80px] bg-black text-white justify-between items-center px-[20px]">
-        <img src={logo} className="App-logo w-[70px] h-[70px]" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="text-[#eee]"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
     </div>
   );
 }
